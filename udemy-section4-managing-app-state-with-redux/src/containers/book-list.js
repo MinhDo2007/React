5f32f0b46323f1fbbdb2770d3d12d0fae1870d7d
MinhDo2007 <minhdt.cntt@gmail.com>
2018-05-08 23:43:49 +0700

    'continue-section-4-and-create-section-5-weather'

--- udemy-section4-managing-app-state-with-redux/src/containers/book-list.js
+++ udemy-section4-managing-app-state-with-redux/src/containers/book-list.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
-export default class BookList extends Component {
+import {connect} from 'react-redux';
+import { selectBook } from './../actions/index';
+import { bindActionCreators } from 'redux';
+
+class BookList extends Component {
   renderList(){
     return this.props.books.map((book)=>{
       return(
-        <li className="list-group-item" key={book.title}>
+        <li className="list-group-item" key={book.title} onClick={()=>this.props.selectBook(book)}>
           {book.title}
         </li>
       )
@@ -11,10 +15,22 @@ export default class BookList extends Component {
   }
 
   render() {
-    return (){
+    return (
       <ul className='list-group col-sm-4'>
         {this.renderList()}
       </ul>
-    }
+    )
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    books: state.books
+  }
+}
+
+function mapDispatchToProps(dispatch){
+  return bindActionCreators({selectBook: selectBook}, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
